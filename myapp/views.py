66a778492f776f887fd0e46eb33b5773fd629663
Grish Gautam <grishgautam17@gmail.com>
2022-12-09 12:25:34 +0545

    little bit of variable added

--- myapp/views.py
+++ myapp/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html")
+    name = "grish"
+    return render(request,"index.html", {'name': name})
 
