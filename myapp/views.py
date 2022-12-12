5089992e967639ca5cf1ca6e7ef033f40f3d7565
Grish Gautam <grishgautam17@gmail.com>
2022-12-12 20:05:28 +0545

    "made models and that fast, reliable thing in html"

--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 # Create your views here.
 
 def index(request):
-    context = {
-        'name' : 'grish',
-        'age' : 23,
-        'nationality' : "Nepali"
-    }
-    return render(request,"index.html")
+	feature1 = Feature()
+	feature1.id = 0
+	feature1.name = "Fast"
+	feature1.details = "our service is very quick"
 
-from django.shortcuts import render
+	feature2 = Feature()
+	feature2.id = 1
+	feature2.name = "Easy to use and"
+	feature2.details = "our service is very Easy to use"
+
+	feature3 = Feature()
+	feature3.id = 2
+	feature3.name = "Reliable"
+	feature3.details = "our service is Reliable"
+
+	feature4 = Feature()
+	feature4.id = 3
+	feature4.name = "Affordable"
+	feature4.details = "our service is very affordable"
+	feature = [feature2,feature1,feature3,feature4]
+
+	return render(request,"index.html", {'feature' : feature,})
 
 
 # defining function for wordcounter
